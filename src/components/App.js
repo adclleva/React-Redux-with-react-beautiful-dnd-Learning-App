81380b4faaabfeba4ff69e164b49ccfc492c9e84
ArvinLleva <arvin.lleva@yahoo.com>
2019-12-14 00:28:21 -0800

    Implemented the connection with Redux and React to have the categories be displayed

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import Category from "./Category"
+import { connect } from "react-redux" // this connects the redux to our react
+import JobCard from './JobCard';
 
-function App() {
+function App(props) {
+  const { categories } = props
+
+  const displayCategories = categories.map((category, index) => {
+    return (
+      <Category title={category.title} jobs={category.jobs}/>
+    )
+  })
+
   return (
     <div className="App">
       <h2>Test</h2>
-      <Category title={"wish list"}/>
+      
+      {displayCategories}
+
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = state => ({
+  categories: state.categories // we get the categories from the reducer's given state
+})
+
+export default connect(mapStateToProps)(App); // this creates the connection
